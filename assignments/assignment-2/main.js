1cb56d6e89a1a7f09f00ce6095ba9b5c3c771eb2
oadewole <49413438+oadewole@users.noreply.github.com>
2020-03-31 02:09:43 -0400

    Set button functionality

--- assignments/assignment-2/main.js
+++ assignments/assignment-2/main.js
@@ -16,10 +16,25 @@ for (let i = 1; i <= 5; i++) {
   newImage.setAttribute("src", path);
   thumbBar.appendChild(newImage);
 
-  // adding onClick handler to change to selected image path (not sure why this is works/how to logic the explanation)
+  // adding onClick handler to change to selected image path (not sure why this is works/how to logic the explanation/what the other way to do this would be)
   newImage.onclick = function() {
     displayedImage.setAttribute("src", path);
-  };
+  }
 }
 
 /* Wiring up the Darken/Lighten button */
+btn.onclick = function() {
+    // check current class name set on button
+    let currClass = btn.getAttribute('class');
+
+    // if class name is "dark"
+    if(currClass === 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+}
