032a8110d8cdd14c8f7c5b81d3db342fb65a36f4
oadewole <49413438+oadewole@users.noreply.github.com>
2020-03-31 01:53:19 -0400

    Develop loop and onclick handler

--- assignments/assignment-2/main.js
+++ assignments/assignment-2/main.js
@@ -1,13 +1,25 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+/* 
+Note: I did not use the MDN source code at all for this assignment. However I did look at their descriptions of what the JS should accomplish in order to gain clarity on what was being asked in the assignment description. */
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
 
-/* Looping through images */ 
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+/* Looping through images */
+for (let i = 1; i <= 5; i++) {
+  const newImage = document.createElement("img");
+
+  let path = "images/pic" + i + ".jpg";
+
+  newImage.setAttribute("src", path);
+  thumbBar.appendChild(newImage);
+
+  // adding onClick handler to change to selected image path (not sure why this is works/how to logic the explanation)
+  newImage.onclick = function() {
+    displayedImage.setAttribute("src", path);
+  };
+}
 
 /* Wiring up the Darken/Lighten button */
