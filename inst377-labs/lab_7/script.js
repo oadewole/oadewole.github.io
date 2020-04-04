4b15824e8c446c9526b5de020c76c9e385c47002
oadewole <49413438+oadewole@users.noreply.github.com>
2020-04-03 21:43:05 -0400

    Import dataset using Fetch API
    - title
    - Wrote code to pick 3 random establishments and store their names and coordinates
    - began styling popups

--- inst377-labs/lab_7/script.js
+++ inst377-labs/lab_7/script.js
@@ -1,3 +1,4 @@
+
 const mapOne = L.map('my-map').setView([38.9860, -76.9451], 13); // center map at McKeldin Library
 
 // map of London
@@ -11,4 +12,50 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(mapOne);
 
 const mark1 = L.marker([38.9860, -76.9451]).addTo(mapOne); // marker at Mckeldin
-mark1.bindPopup("<b>McKeldin Library</b>");
+
+let establishments = [];
+
+mark1.bindPopup("<b>McKeldin Library</b>", {className:"lib-popup"}); // popup for McKeldin
+
+// fetch PG Food Inspection dataset
+fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json')
+  .then((response) => { // why is this section necessary?
+    return response.json();
+  })
+  .then((data) => {
+    // console.log(data);
+
+    const establishments = [];
+
+    function randIndex(start, end) { // helper function: random num generator
+        return Math.floor(Math.random() * (end - start) + start);
+      }
+
+    // pick 3 random establishments from dataset
+    for (let i = 0; i < 3; i++) {
+        idx = randIndex(0, data.length);
+  
+        // if selected item is already in list, pick randomly again (ensuring uniqueness...hopefully)
+        while (establishments.includes(data[idx])) {
+          idx = randIndex(0, data.length);
+        }
+  
+        // add establishment to list of establishments (name, lat, and long)
+        establishments.push({
+            "name": data[idx].name, 
+            "lat": data[idx].geocoded_column_1.coordinates[0], 
+            "long": data[idx].geocoded_column_1.coordinates[1]});
+      }
+
+    console.log(establishments);
+
+    // add marker and popup for each establishment
+
+
+  });
+
+//   console.log(establishments);
+
+
+
+// 
