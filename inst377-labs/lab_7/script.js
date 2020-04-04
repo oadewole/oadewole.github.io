c85ad8772ed0cfa10aa08faead05503ea38fd3eb
oadewole <49413438+oadewole@users.noreply.github.com>
2020-04-03 23:06:47 -0400

    Add markers and popups for 3 places

--- inst377-labs/lab_7/script.js
+++ inst377-labs/lab_7/script.js
@@ -1,7 +1,6 @@
 
-const mapOne = L.map('my-map').setView([38.9860, -76.9451], 13); // center map at McKeldin Library
+const mapOne = L.map('my-map').setView([38.9860, -76.9451], 10); // center map at McKeldin and zoom out enough to see all 4 markers
 
-// map of London
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -13,9 +12,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 const mark1 = L.marker([38.9860, -76.9451]).addTo(mapOne); // marker at Mckeldin
 
-let establishments = [];
-
-mark1.bindPopup("<b>McKeldin Library</b>", {className:"lib-popup"}); // popup for McKeldin
+mark1.bindPopup("<b>MCKELDIN LIBRARY</b>", {className:"lib-popup"}); // popup for McKeldin
 
 // fetch PG Food Inspection dataset
 fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json')
@@ -23,9 +20,7 @@ fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json')
     return response.json();
   })
   .then((data) => {
-    // console.log(data);
-
-    const establishments = [];
+    const establishments = []; 
 
     function randIndex(start, end) { // helper function: random num generator
         return Math.floor(Math.random() * (end - start) + start);
@@ -35,7 +30,7 @@ fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json')
     for (let i = 0; i < 3; i++) {
         idx = randIndex(0, data.length);
   
-        // if selected item is already in list, pick randomly again (ensuring uniqueness...hopefully)
+        // if selected place is already picked, pick randomly again 
         while (establishments.includes(data[idx])) {
           idx = randIndex(0, data.length);
         }
@@ -43,19 +38,18 @@ fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json')
         // add establishment to list of establishments (name, lat, and long)
         establishments.push({
             "name": data[idx].name, 
-            "lat": data[idx].geocoded_column_1.coordinates[0], 
-            "long": data[idx].geocoded_column_1.coordinates[1]});
+            "lat": data[idx].geocoded_column_1.coordinates[1], 
+            "long": data[idx].geocoded_column_1.coordinates[0]});
       }
 
     console.log(establishments);
+    console.log(establishments[0]);
 
     // add marker and popup for each establishment
-
-
+    establishments.forEach(e => {
+        let title = "<b>" + e.name + "</b><br>";
+        L.marker([e.lat, e.long]).addTo(mapOne)
+            .bindPopup(title, {className:"lib-popup"});
+        
+    })
   });
-
-//   console.log(establishments);
-
-
-
-// 
