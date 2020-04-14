fdc478b1045a5bb1dd98573697c1ce390f22ef07
oadewole <49413438+oadewole@users.noreply.github.com>
2020-04-13 22:54:35 -0400

    Entered data source in server.js

--- inst377-labs/lab_9/server.js
+++ inst377-labs/lab_9/server.js
@@ -22,7 +22,7 @@ app.use(express.static('public'));
 
 
 function processDataForFrontEnd(req, res) {
-  const baseURL = ''; // Enter the URL for the data you would like to retrieve here
+  const baseURL = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'; // Enter the URL for the data you would like to retrieve here
 
   // Your Fetch API call starts here
   // Note that at no point do you "return" anything from this function -
